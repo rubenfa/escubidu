d7c24af9c0caf2847562a3818fb7665866255306
Rubén Chavarría <txingo@gmail.com>
2017-02-05 00:05:38 +0100

    Show in a list locations retrieved from the browser

--- apps/web/web/js/geolocation.js
+++ apps/web/web/js/geolocation.js
@@ -1,18 +1,34 @@
 $(document).ready(function() {
 
-    function showCurrentPosition(location) {
-        const latitude = location.coords.latitude;
-        const longitude = location.coords.longitude;
-
+    function showCurrentPosition({ latitude, longitude }) {
         // adds the location to the map (`map` is a global variable)
         L.marker([ latitude, longitude ]).addTo(map);
+    }
+
+    function addLocationToList({ latitude, longitude }) {
+        const li = document.createElement('li');
+        const text = document.createTextNode(`Lat: ${latitude}, Long: ${longitude}`);
+        li.appendChild(text);
+
+        const ul = document.getElementById('locations');
+        ul.appendChild(li);
+    }
+
+    function onCurrentPositionChanged(location) {
+        const locationCoords = {
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude
+        };
+
+        showCurrentPosition(locationCoords);
+        addLocationToList(locationCoords);
     }
 
     function onButtonGeolocateClick(event) {
         event.preventDefault();
 
         if (navigator && navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showCurrentPosition);
+            navigator.geolocation.getCurrentPosition(onCurrentPositionChanged);
         } else {
             alert('No se puede usar geolocalización en este navegador');
         }
