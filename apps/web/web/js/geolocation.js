9101f75297917a035d3b71b42dc6acf9eeeac364
Rubén Chavarría <txingo@gmail.com>
2017-02-05 00:35:10 +0100

    Users can start/stop watching their locations

--- apps/web/web/js/geolocation.js
+++ apps/web/web/js/geolocation.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
 
+    let locationId = 0,
+        locationWatchId,
+        watching = false,
+        btnGeolocate;
+
     function showCurrentPosition({ latitude, longitude }) {
         // adds the location to the map (`map` is a global variable)
         L.marker([ latitude, longitude ]).addTo(map);
     }
 
     function addLocationToList({ latitude, longitude }) {
+        locationId++;
+
         const li = document.createElement('li');
-        const text = document.createTextNode(`Lat: ${latitude}, Long: ${longitude}`);
+        const text = document.createTextNode(`(#${locationId}) Lat: ${latitude}, Long: ${longitude}`);
         li.appendChild(text);
 
         const ul = document.getElementById('locations');
-        ul.appendChild(li);
+        ul.insertBefore(li, ul.firstChild);
     }
 
     function onCurrentPositionChanged(location) {
@@ -24,20 +31,37 @@ $(document).ready(function() {
         addLocationToList(locationCoords);
     }
 
+    function stopWatching() {
+        wathching = false;
+        btnGeolocate.innerText = 'Start';
+        navigator.geolocation.clearWatch(locationWatchId);
+    }
+
+    function startWatching() {
+        watching = true;
+        btnGeolocate.innerText = 'Stop';
+        locationWatchId = navigator.geolocation.watchPosition(onCurrentPositionChanged);
+    }
+
     function onButtonGeolocateClick(event) {
         event.preventDefault();
 
-        if (navigator && navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(onCurrentPositionChanged);
-        } else {
+        if (!navigator || !navigator.geolocation) {
             alert('No se puede usar geolocalización en este navegador');
+            return;
         }
+
+        if (watching) {
+            stopWatching();
+            return;
+        }
+
+        startWatching();
     }
 
-    debugger
-
     // add event listeners to the buttons so that they can perform some actions
-    const btnGeolocate = document.getElementById('geolocate');
+    btnGeolocate = document.getElementById('geolocate');
+    btnGeolocate.innerText = 'Start';
     btnGeolocate.addEventListener('click', onButtonGeolocateClick);
 
 });
