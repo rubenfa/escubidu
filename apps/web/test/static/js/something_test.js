de5a6a8963849958633d1dac996ea3d04ca544d9
Rubén Chavarría <txingo@gmail.com>
2017-02-28 21:30:31 +0100

    Replace `mocha` with `jasmine` as the JavaScript test framework

--- apps/web/test/static/js/something_test.js
+++ apps/web/test/static/js/something_test.js
@@ -1,8 +1,9 @@
-import assert from 'assert';
 import something from '../../../web/static/js/something'
 
 describe('something()', function() {
+  
   it('does something', function () {
-    assert.equal("something", something());
+    expect('something').toEqual(something());
   });
+
 });
