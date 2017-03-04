f04d4d815f5271c9d4eb55ff1f0046b126651ca6
Rubén Chavarría <txingo@gmail.com>
2017-03-04 17:40:12 +0100

    Delete useless tests
    They were used to demonstrate how tests could be done. Now, that there
    are tests for `ExampleMarkers`, they're not needed any more.

--- apps/web/test/static/js/something_test.js
+++
@@ -1,9 +0,0 @@
-import something from '../../../web/static/js/something'
-
-describe('something()', function() {
-  
-  it('does something', function () {
-    expect('something').toEqual(something());
-  });
-
-});
