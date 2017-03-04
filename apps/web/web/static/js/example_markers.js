62c0a9cfdf47399c4cf332a1afa3ff29bfb4c9b1
Rubén Chavarría <txingo@gmail.com>
2017-03-04 17:26:06 +0100

    Delete unused code

--- apps/web/web/static/js/example_markers.js
+++ apps/web/web/static/js/example_markers.js
@@ -3,25 +3,6 @@ import L from 'leaflet';
 /**
  * Render some markers to learn how to render them
  */
-function renderIntoMap(map) {
-    var checa = [ 40.588843, -1.789702 ];
-    var madrid = [ 40.416887, -3.703489 ];
-    var valladolid = [ 41.652271, -4.728628 ];
-
-    var markers = [
-        checa,
-        madrid,
-        valladolid
-    ];
-
-    markers.forEach(function(marker) {
-        L.marker(marker).addTo(map);
-    });
-}
-
-/**
- * Render some markers to learn how to render them
- */
 function renderInto(map) {
     var checa = [ 40.588843, -1.789702 ];
     var madrid = [ 40.416887, -3.703489 ];
@@ -36,7 +17,7 @@ function renderInto(map) {
     map.addMarkers(markers);
 }
 
-const ExampleMarkers = { renderIntoMap, renderInto };
+const ExampleMarkers = { renderInto };
 
 export default ExampleMarkers;
 
