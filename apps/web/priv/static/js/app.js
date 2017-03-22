af0d62651af2823221f7e43e7b7f0168dae86526
Rubén Chavarría <txingo@gmail.com>
2017-03-22 22:16:13 +0100

    Add `Channel` class, to send and receive messages to server
    It has a method, `send`, that will send a message to the server. Clients
    of this class can register as listeners, so any received message will be
    passed to them

--- apps/web/priv/static/js/app.js
+++ apps/web/priv/static/js/app.js
@@ -14822,87 +14822,60 @@ simulator.addListener(listListener);
 });
 
 require.register("web/static/js/communication/channel.js", function(exports, require, module) {
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _phoenix = require("phoenix");
+var _phoenix = require('phoenix');
 
-var socket = new _phoenix.Socket("/locations_socket", { params: { token: window.userToken } });
+function notify(payload, listeners) {
+  listeners.forEach(function (listener) {
+    return listener.onMessageReceived(payload.body);
+  });
+}
 
-// When you connect, you'll often need to authenticate the client.
-// For example, imagine you have an authentication plug, `MyAuth`,
-// which authenticates the session and assigns a `:current_user`.
-// If the current user exists you can assign the user's token in
-// the connection for use in the layout.
-//
-// In your "web/router.ex":
-//
-//     pipeline :browser do
-//       ...
-//       plug MyAuth
-//       plug :put_user_token
-//     end
-//
-//     defp put_user_token(conn, _) do
-//       if current_user = conn.assigns[:current_user] do
-//         token = Phoenix.Token.sign(conn, "user socket", current_user.id)
-//         assign(conn, :user_token, token)
-//       else
-//         conn
-//       end
-//     end
-//
-// Now you need to pass this token to JavaScript. You can do so
-// inside a script tag in "web/templates/layout/app.html.eex":
-//
-//     <script>window.userToken = "<%= assigns[:user_token] %>";</script>
-//
-// You will need to verify the user token in the "connect/2" function
-// in "web/channels/user_socket.ex":
-//
-//     def connect(%{"token" => token}, socket) do
-//       # max_age: 1209600 is equivalent to two weeks in seconds
-//       case Phoenix.Token.verify(socket, "user socket", token, max_age: 1209600) do
-//         {:ok, user_id} ->
-//           {:ok, assign(socket, :user, user_id)}
-//         {:error, reason} ->
-//           :error
-//       end
-//     end
-//
-// Finally, pass the token on connect as below. Or remove it
-// from connect if you don't care about authentication.
+function Channel() {
+  this.connected = false;
+  this.channel = null;
+  this.listeners = [];
+}
 
-// NOTE: The contents of this file will only be executed if
-// you uncomment its entry in "web/static/js/app.js".
+Channel.prototype.init = function () {
+  var _this = this;
 
-// To use Phoenix channels, the first step is to import Socket
-// and connect at the socket path in "lib/my_app/endpoint.ex":
-socket.connect();
+  var socket = new _phoenix.Socket('/locations_socket', { params: { token: window.userToken } });
+  socket.connect();
 
-// Now that you are connected, you can join channels with a topic:
-var channel = socket.channel("location:all", {});
-channel.join().receive("ok", function (resp) {
-  console.log("Joined successfully", resp);
-}).receive("error", function (resp) {
-  console.log("Unable to join", resp);
-});
+  this.channel = socket.channel('location:all', {});
+  this.channel.join().receive('ok', function (resp) {
+    _this.connected = true;
+    console.log('Joined successfully', resp);
+  }).receive('error', function (resp) {
+    _this.connected = false;
+    console.log('Unable to join', resp);
+  });
 
-// send a msg after 3sg
-setTimeout(function () {
-  channel.push('new_msg', { body: 'Body of the message' });
-  console.log('msg sent');
-}, 3000);
+  this.channel.on('location_message', function (payload) {
+    notify(payload, _this.listeners);
+  });
+};
 
-// show received msgs in the console
-channel.on('new_msg', function (payload) {
-  console.log('He recibido un cuerpo de msg:', payload.body);
-});
+Channel.prototype.send = function (body) {
+  if (!this.connected) {
+    throw new Error('Must be connected to send messages');
+  }
 
-exports.default = socket;
+  var message = { body: body };
+  this.channel.push('location_message', message);
+};
+
+Channel.prototype.addListener = function (listener) {
+  this.listeners.add(listener);
+};
+
+exports.default = Channel;
 });
 
 require.register("web/static/js/geolocation/browser_geolocation.js", function(exports, require, module) {
